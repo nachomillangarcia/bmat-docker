c4af0e29b903b37c6668d4d15b2f51e9129a2321
Ignacio Millan <ignacio@awardforce.com>
2020-11-06 19:08:48 +0100

    Update flask-app

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
-
+    return 'Hey, we have Flask in a Docker container!!!!'
 
 app.run(debug=True, host='0.0.0.0')
